2426aa1e9fa6b264508978569936001e77750900
Bartosz Hejman <bartoszhejman@gmail.com>
2017-11-12 23:14:09 +0100

    Update Procfile

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,7 +7,7 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
-  config.redis = { :size => 9 }
+  config.redis = { :size => 22 }
 end
 
 unless Rails.env.development?
