43486cd022c9dc1f41016b91f2806d86c674be4d
Bartosz Hejman <bartoszhejman@gmail.com>
2017-11-12 22:45:22 +0100

    Update sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,12 @@ require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { :size => 1 }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { :size => 9 }
 end
 
 unless Rails.env.development?
