1b98984c363a05dce347aaba451fb253b2685708
Bartosz Hejman <bartoszhejman@gmail.com>
2017-11-12 23:21:06 +0100

    Update default_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3232 }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => ENV['MAILTRAP_USERNAME'],
