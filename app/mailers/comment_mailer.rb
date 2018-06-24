6d1020b103b71294cdfce0c9ca7f294f2d806c15
Bartosz Hejman <bartoszhejman@gmail.com>
2018-06-25 00:33:19 +0200

    Fix comment mailer error

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -5,7 +5,7 @@ class CommentMailer < ApplicationMailer
 
     users = User.joins(:comments).where('comments.commentable_id = :id AND comments.commentable_type = :type AND users.id != :author_id', { id: @comment.commentable_id, type: @comment.commentable_type, author_id: @comment.author_id })
 
-    mail(to: users.map(&:email), from: @comment.author.email, subject: subject)
+    mail(to: users.map(&:email), from: @comment.author.email, subject: subject) if users.any?
   end
 
   private
