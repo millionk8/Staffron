839a730a0eebcdff332ddb0ac88650f651de01d0
Bartosz Hejman <bartoszhejman@gmail.com>
2019-03-03 21:00:05 +0100

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,8 @@ Rails.application.configure do
       :user_name => ENV['SMTP_USERNAME'],
       :password => ENV['SMTP_PASSWORD'],
       :authentication => :login,
-      :enable_starttls_auto => true
+      :enable_starttls_auto => true,
+      :tls => true
   }
 
   config.paperclip_defaults = {
