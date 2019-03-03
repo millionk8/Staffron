487ed648ae1628bf35111587f35abcf7eade8865
Bartosz Hejman <bartoszhejman@gmail.com>
2019-03-03 20:27:49 +0100

    Update config files for staging and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_controller.default_url_options = { host: ENV['MAILER_URL'] }
   config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
+  Rails.application.routes.default_url_options[:host] = ENV['MAILER_URL']
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
