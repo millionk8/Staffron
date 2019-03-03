98cfb68d241b8bdb0d7bc3d7f47bff09430b214f
Bartosz Hejman <bartoszhejman@gmail.com>
2019-03-03 21:42:48 +0100

    Update users table to fix password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211222440) do
+ActiveRecord::Schema.define(version: 20190303204126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(version: 20180211222440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "policy_accepted_at"
+    t.boolean "allow_password_change", default: false, null: false
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
