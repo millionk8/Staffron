59ee53ef6420f09015618ff2737cd8ff51a8ad9a
Bartosz Hejman <bartoszhejman@gmail.com>
2019-03-03 20:47:48 +0100

    Update production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,11 +87,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['MAILER_SMTP_USERNAME'],
-      :password => ENV['MAILER_SMTP_PASSWORD'],
-      :address => ENV['MAILER_SMTP_ADDRESS'],
-      :domain => ENV['MAILER_SMTP_DOMAIN'],
-      :port => ENV['MAILER_SMTP_PORT'].to_i,
+      address => ENV['SMTP_ADDRESS'],
+      :port => ENV['SMTP_PORT'],
+      :user_name => ENV['SMTP_USERNAME'],
+      :password => ENV['SMTP_PASSWORD'],
       :authentication => :login,
       :enable_starttls_auto => true
   }
