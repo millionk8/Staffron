057dc341a37a36ed160517ee7c7fbba6131fa5ac
Bartosz Hejman <bartoszhejman@gmail.com>
2019-03-03 20:54:30 +0100

    Fix bug in production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address => ENV['SMTP_ADDRESS'],
+      :address => ENV['SMTP_ADDRESS'],
       :port => ENV['SMTP_PORT'],
       :user_name => ENV['SMTP_USERNAME'],
       :password => ENV['SMTP_PASSWORD'],
