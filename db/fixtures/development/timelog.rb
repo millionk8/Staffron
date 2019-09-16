3aa92bfe54bd185f3710124ac615fc2bd6ffa32b
Kris <kris@moonrisers.com>
2019-09-16 15:22:06 -0500

    fix for admin to update users timelog

---
+++ db/fixtures/development/timelog.rb
@@ -0,0 +1,17 @@
+TimeLog.seed(:id,
+	{
+		id: 1, 
+		user_id: 1, 
+		category_id: 1, 
+		started_at: "2019-09-16 06:00:00", 
+		stopped_at: "2019-09-16 14:00:00", 
+		note: "This is a great note.", 
+		deleted: false, 
+		deleted_at: nil, 
+		created_at: "2019-09-16 21:03:59", 
+		updated_at: "2019-09-16 21:03:59", 
+		custom: true, 
+		actual_started_at: nil, 
+		actual_stopped_at: nil
+	}
+)
