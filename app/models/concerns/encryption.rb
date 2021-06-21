763cff31f1275549f4357c1d320ea40f0c7f32d1
uppay8 <uppay8@gmail.com>
2021-06-21 09:48:34 +0500

    fixes encryption key issue: ArgumentError: key must be 32 bytes

--- app/models/concerns/encryption.rb
+++ app/models/concerns/encryption.rb
@@ -6,7 +6,7 @@ module Encryption
   def encryption_key
     if Rails.env.production?
       raise 'Must set secret key!!' unless ENV['SECRET_KEY']
-      ENV['SECRET_KEY']
+      ENV['SECRET_KEY'].bytes[0..31].pack( "c" * 32 )
     else
       ENV['SECRET_KEY'] ? ENV['SECRET_KEY'] : 'test_key'
     end
